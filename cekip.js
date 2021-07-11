5741a37db2e43a253453bbf7b81e059b81551803
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-07-11 19:18:44 +0700

    update cekip.js

--- cekip.js
+++ cekip.js
@@ -1,10 +1,32 @@
-window.setTimeout("getip()",1000);
+function addip(){ 
+    var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://api.countapi.xyz/create?key="+ipanda+"&namespace=ip-"+location.host+"&value=15");
+	xhr.responseType = "json";
+	xhr.onload = function() {
+    	sisaip = this.response.value
+	}
+	xhr.send();
+}
+
+function countip(){ 
+    var xhr = new XMLHttpRequest();
+	xhr.open("GET", `https://api.countapi.xyz/info/ip-${location.host}/${ipanda}`);
+	xhr.responseType = "json";
+	xhr.onload = function() {
+		if (!this.response.value) return addip() 
+    	sisaip = this.response.value
+	}
+	xhr.send();
+}
+
 function getip(){ 
     var xhr = new XMLHttpRequest();
 	xhr.open("GET", `https://api.ipify.org/?format=json`);
 	xhr.responseType = "json";
 	xhr.onload = function() {
     	ipanda = this.response.ip
+    	countip()
 	}
 	xhr.send();
 }
+getip()
