fc2eb3c54e588ca3d116fd2189841bf0b76b38fe
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-07-12 15:07:21 +0700

    update baterai.js

---
+++ baterai.js
@@ -0,0 +1,43 @@
+charging = false
+function animasicas(){
+	if (!charging) return console.log('')
+	document.getElementById('sisabatre').style.width = 100+'%'
+	setTimeout("document.getElementById('sisabatre').style.width = sisabatre", 1000);
+	setTimeout("animasicas()", 2000)
+}
+
+navigator.getBattery().then(function(battery) {
+  function updateAllBatteryInfo(){
+    updateLevelInfo();
+    updateChargeInfo();
+  }
+  updateAllBatteryInfo();
+
+  battery.addEventListener('chargingchange', function(){
+    updateChargeInfo();
+  });
+  function updateChargeInfo(){
+    (battery.charging ? Swal.fire('', `Selamat mengisi baterai<br>yang tersisa ${battery.level * 100}%`) : "")
+	if (battery.charging) {
+	charging = true
+	statuscas = 'tersambung'
+	animasicas()
+	}else{
+	charging = false
+	statuscas = 'terputus'
+	document.getElementById('sisabatre').style.width = sisabatre
+	}
+    console.log('charging: '+battery.charging)
+  }
+
+  battery.addEventListener('levelchange', function(){
+    updateLevelInfo();
+  });
+  function updateLevelInfo(){
+  	document.getElementById('sisabatre').style.width = battery.level * 100 + "%"
+  	document.getElementById('sisabatree').innerText = battery.level * 100 + "%"
+  	sisabatre = battery.level * 100 + "%"
+    console.log("Battery level: "
+                + battery.level * 100 + "%");
+  }
+});
