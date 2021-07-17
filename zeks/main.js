7556cfc90f7024cafc5170dd4f112e0da7ca0588
Fauzan Rifki Maulana <80396521+frmdeveloper@users.noreply.github.com>
2021-07-17 11:50:19 +0700

    update main.js

--- zeks/main.js
+++ zeks/main.js
@@ -50,7 +50,7 @@ if (query.type){
     };
             $("#cekkeyform").submit(function(e) {
                 $.ajax({
-                    url: '/apikey?apikey='+apikey.value,
+                    url: 'https://zeks.xyz/apikey?apikey='+apikey.value,
                     type: 'GET',
                     beforeSend: function() {
 $("#sbm").html(`<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
